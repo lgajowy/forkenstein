8443a7837015c810370152c9503b6534b288f921
Ronald Wang <ronaldwang88@gmail.com>
2022-08-09 15:51:27 -0400

    initial commit - mainly changing 2.0.0-RC6 to 2.0.0

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,15 @@
 import zio._
+import zio.metrics.jvm.DefaultJvmMetrics
 
 object Main extends ZIOAppDefault {
-  override def run: ZIO[Any, Nothing, Boolean] = {
-    Program.findTheMeaningOfLife(3, Int.MinValue, Int.MaxValue).provideLayer(Runtime.trackRuntimeMetrics)
+
+  override def run: ZIO[Any, Throwable, Boolean] = {
+    Program
+      .findTheMeaningOfLife(3, Int.MinValue, Int.MaxValue)
+      .provide(
+        Runtime.enableRuntimeMetrics, //NOTE: refactored by following this zio-metrics-connectors example (https://github.com/zio/zio-metrics-connectors/blob/01c438153fc2c358caef163731150306e9939cde/core/jvm/src/test/scala/zio/metrics/connectors/SampleApp.scala#L69-L70)
+        DefaultJvmMetrics.live.unit   //NOTE: DefaultJvmMetrics.live collects the same JVM metrics as the Prometheus Java client's default exporters
+      )
   }
+
 }
