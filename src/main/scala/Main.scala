ce557492b468aeb26ad5c3e326e30432177f1941
Łukasz Gajowy <lukasz.gajowy@gmail.com>
2022-08-10 10:34:19 +0200

    Update src/main/scala/Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,8 +7,8 @@ object Main extends ZIOAppDefault {
     Program
       .findTheMeaningOfLife(3, Int.MinValue, Int.MaxValue)
       .provide(
-        Runtime.enableRuntimeMetrics, //NOTE: refactored by following this zio-metrics-connectors example (https://github.com/zio/zio-metrics-connectors/blob/01c438153fc2c358caef163731150306e9939cde/core/jvm/src/test/scala/zio/metrics/connectors/SampleApp.scala#L69-L70)
-        DefaultJvmMetrics.live.unit   //NOTE: DefaultJvmMetrics.live collects the same JVM metrics as the Prometheus Java client's default exporters
+        Runtime.enableRuntimeMetrics, // NOTE: refactored by following this zio-metrics-connectors example (https://github.com/zio/zio-metrics-connectors/blob/01c438153fc2c358caef163731150306e9939cde/core/jvm/src/test/scala/zio/metrics/connectors/SampleApp.scala#L69-L70)
+        DefaultJvmMetrics.live.unit   // NOTE: DefaultJvmMetrics.live collects the same JVM metrics as the Prometheus Java client's default exporters
       )
   }
 
